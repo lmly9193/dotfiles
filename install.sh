47f819333a55a89f64a63dad99aae970629c8789
Ethan <15520160+lmly9193@users.noreply.github.com>
2020-11-17 22:02:53 +0000

    update

--- install.sh
+++ install.sh
@@ -5,13 +5,15 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y exa screen virt-what unzip neofetch git curl wget htop thefuck
 
+if ! dpkg-query -W -f='${Status}' exa  | grep "ok installed"; then sudo cp ~/dotfiles/exa-linux-x86_64 /usr/local/bin/exa; fi
 
 cd ~/
-
-if ! dpkg-query -W -f='${Status}' exa  | grep "ok installed"; then sudo cp ~/dotfiles/exa-linux-x86_64 /usr/local/bin/exa && sudo chmod +x /usr/local/bin/exa; fi
 
 echo '
 . ~/dotfiles/.profile' >> ~/.profile
 
 echo '
 . ~/dotfiles/.aliases' >> ~/.bashrc
+
+. ~/dotfiles/.profile
+. ~/dotfiles/.aliases
